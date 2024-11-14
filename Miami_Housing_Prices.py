929cd655fbc57485ecfc07e1f3068c7424d1a577
VenkateshKannan1999 <166029145+VenkateshKannan1999@users.noreply.github.com>
2024-11-14 18:52:11 +0100

    Update Miami_Housing_Prices.py

--- Miami_Housing_Prices.py
+++ Miami_Housing_Prices.py
@@ -44,7 +44,7 @@ input_data = pd.DataFrame({
         'month_sold': [month_sold], 
         'structure_quality': [structure_quality]
     })
-  if st.button("Predict"):
+if st.button("Predict"):
     with st.spinner('Calculating...'):  # Display a spinner while predicting
       time.sleep(1)  # Simulate some processing time
       prediction = model.predict(input_data)[0]
