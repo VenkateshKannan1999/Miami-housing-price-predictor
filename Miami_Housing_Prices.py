61e6fed8f2c1647d1da448c03d1c0a75c0c446a0
VenkateshKannan1999 <166029145+VenkateshKannan1999@users.noreply.github.com>
2024-11-14 19:29:25 +0100

    Update Miami_Housing_Prices.py

--- Miami_Housing_Prices.py
+++ Miami_Housing_Prices.py
@@ -43,9 +43,8 @@ def main():
     })
     if st.button("Predict"):
         with st.spinner('Calculating...'):  # Display a spinner while predicting
-          time.sleep(1)  # Simulate some processing time
-          prediction = model.predict(input_data)[0]
-          st.success(f"Predicted Price: ${prediction:,.2f}")
-          st.balloons()  # Show balloons after prediction
+            prediction = model.predict(input_data)[0]
+            st.success(f"Predicted Price: ${prediction:,.2f}")
+            st.balloons()  # Show balloons after prediction
 if __name__ == "__main__":
   main()
